577ae5127852249c315fb9262f78c02928d5b015
zk <liuzq7@gmail.com>
2020-07-23 01:21:13 +0800

    create the graph

--- mywebpack.js
+++ mywebpack.js
@@ -1,15 +1,44 @@
-const fs = require("fs");
-const babylon = require("babylon");
+const fs       = require("fs");
+const babylon  = require("babylon");
+const path     = require('path');
 const traverse = require("babel-traverse").default;
+
+let ID = 0;
+
 function createAsset(filename){
   const content = fs.readFileSync(filename,'utf-8');
   const ast= babylon.parse(content,{sourceType:'module'})
-  //console.log(ast);
+
+  const dependencies = [];
   traverse(ast,{
     ImportDeclaration:({node})=>{
-      console.log(node);
+      dependencies.push(node.source.value);
     }
   });
+
+  const id =ID++;
+
+  return {
+    id,
+    filename,
+    dependencies
+  }
 }
 
-createAsset('./entry.js');
+function createGraph(entry){
+  const mainAsset = createAsset(entry);
+  const queue = [mainAsset];
+  for(const asset of queue){
+    const dirname = path.dirname(asset.filename);
+    asset.mapping ={};
+    asset.dependencies.forEach(relativePath => {
+      const absolutePath = path.join(dirname,relativePath);
+      const child = createAsset(absolutePath);
+      asset.mapping[relativePath] = child.id;
+      queue.push(child);
+    });
+  }
+  return queue;
+}
+const graph = createGraph('./entry.js');
+console.log(graph);
